285002a5a5e8f9d2138c7fffe2d54dc84cad9dc5
Chris Woolcott <chris.woolcott@quotacy.com>
2022-02-04 21:33:57 -0600

    test

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 //mongodb://localhost/schools-db
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://sample:sample@cluster0.1jgft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  //process.env.MONGODB_URI || "mongodb+srv://sample:sample@cluster0.1jgft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb://localhost/schools-db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
